5250e510beb628cb5be685c575793f83da5ed449
Armankabirr <armanutsho12@gmail.com>
2026-01-15 16:21:36 +0600

    Update portfolio files

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
 
 function opentab(tabname) {
-    for(tablink of tablinks) {
+    for (tablink of tablinks) {
         tablink.classList.remove("active-link");
     }
-    for(tabcontent of tabcontents) {
+    for (tabcontent of tabcontents) {
         tabcontent.classList.remove("active-tab");
     }
     event.currentTarget.classList.add("active-link");
@@ -20,3 +20,22 @@ function openmenu() {
 function closemenu() {
     navbar.style.right = "-200px";
 }
+
+
+
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzILvmKS_k7-o9XWmQojPd6ndtBOHGb9tTsUOul7Px81-tiNiKSKLjlbGiNblpDGW_r/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg");
+form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            msg.innerHTML = "Message sent successfully";
+            setTimeout(function () {
+                msg.innerHTML = "";
+            }, 5000);
+            form.reset();
+        })
+        .catch(error => console.error('Error!', error.message))
+})
