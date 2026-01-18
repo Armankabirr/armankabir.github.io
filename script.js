895425ab4c1fec276c627f160c05fd9ce0e83c3a
Armankabirr <armanutsho12@gmail.com>
2026-01-18 23:28:08 +0600

    Fix navbar scroll blur effect and enhance visibility

--- script.js
+++ script.js
@@ -21,6 +21,18 @@ function closemenu() {
     navbar.style.right = "-200px";
 }
 
+// Navbar scroll effect
+window.addEventListener('scroll', function() {
+    const nav = document.querySelector('nav');
+    if (nav) {
+        if (window.scrollY > 50) {
+            nav.classList.add('scrolled');
+        } else {
+            nav.classList.remove('scrolled');
+        }
+    }
+});
+
 
 
 
